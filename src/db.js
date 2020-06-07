049dca8ed85c41f756ba230fb71577d07c63de41
Didier Catz <didierlennon@gmail.com>
2020-06-07 18:03:02 +0200

    Add JSDoc

--- src/db.js
+++ src/db.js
@@ -11,142 +11,177 @@ const URI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_URL}/${DB_NAME}?retryW
 
 
 const connect = async () => MongoClient
-  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .catch(err => { throw err })
+	.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.catch(err => { throw err })
 
 /**
+ * Read data from the database
+ * @function Read
+ * @param {Object} options - The options to pass to the function
+ * @param {String} options.collection - The name of the collection
+ * @param {{}} options.query - The query to execute on the collection
+ * @param {Number} options.amount - The amount of results to return
  * @returns A promise that resolves with the data
- */
+*/
 async function Read({ collection = '', query = {}, amount = 0 }) {
-  // Connect to the database
-  const client = await connect()
+	// Connect to the database
+	const client = await connect()
 
-  try {
-    // Fetch the response
-    const response = await client.db(DB_NAME)
-      .collection(collection)
-      .find(query)
-      .limit(amount)
-      .toArray()
+	try {
+		// Fetch the response
+		const response = await client.db(DB_NAME)
+			.collection(collection)
+			.find(query)
+			.limit(amount)
+			.toArray()
 
-    // Return the response
-    return response
-  } catch (err) {
+		// Return the response
+		return response
+	} catch (err) {
 
-    // Catch and throw an error if it occurs
-    throw err
-  } finally {
+		// Catch and throw an error if it occurs
+		throw err
+	} finally {
 
-    // Close connection to the client
-    client.close()
-  }
+		// Close connection to the client
+		client.close()
+	}
 }
 
+/**
+ * Create data in the database
+ * @function Create
+ * @param {Object} options - The options to pass to the function
+ * @param {String} options.collection - The name of the collection
+ * @param {{}} options.data - The data to create
+*/
 async function Create({ collection = '', data = {} }) {
-  // Connect to the database
-  const client = await connect()
+	// Connect to the database
+	const client = await connect()
 
-  try {
-    // Fetch the response
-    if (Array.isArray(data) === true) {
-      await client.db(DB_NAME)
-        .collection(collection)
-        .insertMany(data)
-    } else {
-      await client.db(DB_NAME)
-        .collection(collection)
-        .insertOne(data)
-    }
+	try {
+		// Fetch the response
+		if (Array.isArray(data) === true) {
+			await client.db(DB_NAME)
+				.collection(collection)
+				.insertMany(data)
+		} else {
+			await client.db(DB_NAME)
+				.collection(collection)
+				.insertOne(data)
+		}
 
-    // Return the response
-    return
-  } catch (err) {
+		// Return the response
+		return
+	} catch (err) {
 
-    // Catch and throw an error if it occurs
-    throw err
-  } finally {
+		// Catch and throw an error if it occurs
+		throw err
+	} finally {
 
-    // Close connection to the client
-    client.close()
-  }
+		// Close connection to the client
+		client.close()
+	}
 }
 
-async function Update({ collection = '', query = {}, data = {} }) {
 
-  // Check if data is not empty
-  if (!data || data === {}) {
-    throw new Error(`Can't update data with empty data. If you want to remove data, use the delete function.`)
-  }
+/**
+ * Update data in the database
+ * @function Update
+ * @param {Object} options - The options to pass to the function
+ * @param {String} options.collection - The name of the collection
+ * @param {{}} options.query - The query to execute on the collection
+ * @param {{}} options.data - The data to update
+*/
+async function Update({
+	collection = '',
+	query = {},
+	data = {}
+}) {
 
-  // Connect to the database
-  const client = await connect()
+	// Check if data is not empty
+	if (!data || data === {}) {
+		throw new Error(`Can't update data with empty data. If you want to remove data, use the delete function.`)
+	}
 
-  try {
+	// Connect to the database
+	const client = await connect()
 
-    // Check if data is an arroy or an object
-    if (Array.isArray(data) === true) {
-      await client.db(DB_NAME)
-        .collection(collection)
-        .updateMany(query, {
-          $set: data
-        })
-    } else {
-      await client.db(DB_NAME)
-        .collection(collection)
-        .updateOne(query, {
-          $set: data
-        })
-    }
+	try {
 
-    console.log(`Set data to ${data}`)
+		// Check if data is an arroy or an object
+		if (Array.isArray(data) === true) {
+			await client.db(DB_NAME)
+				.collection(collection)
+				.updateMany(query, {
+					$set: data
+				})
+		} else {
+			await client.db(DB_NAME)
+				.collection(collection)
+				.updateOne(query, {
+					$set: data
+				})
+		}
 
-    // Return the response
-    return
-  } catch (err) {
+		console.log(`Set data to ${data}`)
 
-    // Catch and throw an error if it occurs
-    throw err
+		// Return the response
+		return
+	} catch (err) {
 
-  } finally {
+		// Catch and throw an error if it occurs
+		throw err
 
-    // Close connection to the client
-    client.close()
+	} finally {
 
-  }
+		// Close connection to the client
+		client.close()
+
+	}
 }
 
+
+/**
+ * Delete data from the database
+ * @function Delete
+ * @param {Object} options - The options to pass to the function
+ * @param {String} options.collection - The name of the collection
+ * @param {{}} options.query - The query to execute on the collection
+ * @param {{}} options.data - The data to delete
+*/
 async function Delete({ collection = '', query = {}, data = {} }) {
-  // Connect to the database
-  const client = await connect()
+	// Connect to the database
+	const client = await connect()
 
-  try {
+	try {
 
-    // Check if data is an arroy or an object
-    if (Array.isArray(data) === true) {
-      await client.db(DB_NAME)
-        .collection(collection)
-        // .find(query)
-        .deleteMany(query, data)
-    } else {
-      await client.db(DB_NAME)
-        .collection(collection)
-        // .find(query)
-        .deleteOne(query, data)
-    }
+		// Check if data is an arroy or an object
+		if (Array.isArray(data) === true) {
+			await client.db(DB_NAME)
+				.collection(collection)
+				// .find(query)
+				.deleteMany(query, data)
+		} else {
+			await client.db(DB_NAME)
+				.collection(collection)
+				// .find(query)
+				.deleteOne(query, data)
+		}
 
-    // Return the response
-    return
-  } catch (err) {
+		// Return the response
+		return
+	} catch (err) {
 
-    // Catch and throw an error if it occurs
-    throw err
+		// Catch and throw an error if it occurs
+		throw err
 
-  } finally {
+	} finally {
 
-    // Close connection to the client
-    client.close()
+		// Close connection to the client
+		client.close()
 
-  }
+	}
 }
 
 module.exports = { Create, Read, Update, Delete }
