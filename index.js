9b55b96a3232eec0f72bb0c9321d22f4566690cf
Sybren Molenaar <sybren.molenaar@hva.nl>
2020-06-08 17:55:38 +0200

    Added sign-up feature with hashed password

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const session = require('express-session')
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000
 const { Read, Create } = require('./src/db')
-
+const bcrypt = require('bcrypt')
 const { DB_USER, DB_PASSWORD, DB_URL, DB_NAME } = process.env
 
 const URI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_URL}/${DB_NAME}?retryWrites=true&w=majority`
@@ -30,18 +30,95 @@ app
 		res
 			.status(200)
 			.render('index', {
-				users
+				users,
+				title: 'Dating App'
 			})
 	})
-	.get('/login', (req, res) => { 
-		 res.status(200).render('login')
+	.get('/login', (req, res) => {
+		res.status(200).render('login', {
+			title: 'Login',
+			signUp: false
+		})
 	})
-	.post('/login', async(req, res) => {
+	.get('/signup', (req, res) => {
+		res.status(200).render('login', {
+			title: 'Sign Up',
+			signUp: true
+		})
+	})
+	.post('/login', async (req, res) => {
 		const data = req.body
-				
+
+		const results = await Read({
+			collection: 'users',
+			query: {
+				email: {
+					$eq: data.email
+				}
+			}
+		})
+
+		console.log('Results:', results[0])
+
+		if (results.length === 0) {
+			res.render('login', { signUp: false, errorMessage: 'No user was found with that that e-mail. Would you like to <a href="/signup">sign up?</a>' })
+			return
+		}
+
+		bcrypt.compare(data.pass, results[0].pass, (err, result) => {
+			console.log(result)
+			if (err) { throw err }
+
+			if (result) {
+				res.status(301).redirect('/')
+			} else {
+				res.status(401).redirect('/login')
+			}
+		})
+	})
+	.post('/signup', async (req, res) => {
+		const data = req.body
+		data.age = parseInt(data.age)
+
+		const doesUserExist = await Read({
+			collection: 'users',
+			query: {
+				email: {
+					$eq: data.email
+				}
+			}
+		})
+
+		if (doesUserExist.length > 0) {
+			res.render('login', { signUp: true, errorMessage: 'User already exists with that e-mail. Would you like to <a href="/login">log in?</a>' })
+			return
+		}
+
+
+		bcrypt.hash(data.pass, 10, async (err, hash) => {
+			data.pass = hash
+
+			await Create({
+				collection: 'users',
+				data
+			})
+		})
+
+		res.status(200).redirect('/')
+	})
+
+	.post('/login', async (req, res) => {
+		const data = req.body
+
 		console.log(data)
 
-		await Read({})
+		const user = await Read({
+			collection: 'users', query: {
+				email: req.body.email
+			}
+		})
+
+		console.log(user)
 	})
 	.post('/add', async (req, res) => {
 		const data = req.body
