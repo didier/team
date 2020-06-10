8cfa6027ec77278d8adc06883f0f748a3d596845
Didier Catz <didierlennon@gmail.com>
2020-06-10 18:58:23 +0200

    Add Session secret

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { Session } from 'express-session';
 require('dotenv').config()
 
 const express = require('express')
@@ -6,16 +7,17 @@ const session = require('express-session')
 const bodyParser = require('body-parser')
 const router = require('./src/router')
 
-const port = process.env.PORT || 3000
+const { PORT, NODE_ENV, SESSION_SECRET } = process.env
+const port = PORT || 3000
 
 app
 	.use(express.static('public'))
 	.use(bodyParser.urlencoded({ extended: true }))
-	.use(session({ 'secret': 'vdjifvjdiovjodjvodjfvjodivo' }))
+	.use(session({ 'secret': SESSION_SECRET }))
 	.set('view engine', 'ejs')
 	.set('views', 'src/views')
 	.use(router)
 	.listen(port, () => {
-		console.log(`App is running in ${process.env.NODE_ENV} mode on http://localhost:${port}`)
+		console.log(`App is running in ${NODE_ENV} mode on http://localhost:${port}`)
 		console.log('—————————————————————————————————————————————————————————')
 	})
