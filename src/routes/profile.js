b75cdee8615a005366e70d1dd1f8aad06af41c91
zoeseveriens <zoe.222@hotmail.com>
2020-06-18 14:47:04 +0200

    made updating the profile possible

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,6 +1,32 @@
 
-function getProfilePage (req, res, next) {
-res.status(200).render('profile-page')
+const { Update } = require('../db')
+
+//Dit laat de data van de ingelogde user zien op de profile page
+async function getProfilePage (req, res, next) {
+	console.log(req.session)
+
+	const user = req.session.user
+
+res.status(200).render('profile-page', {user})
+
 }
 
-module.exports = { getProfilePage }
+
+//Wanneer je een input veld aanpast en op save klikt wordt de data in de database aangepast
+const postProfilePage = async (req, res) => {
+	const data = req.body
+	const user = req.session.user
+
+	if (data) {
+		console.log(data)
+	}
+
+	await Update({
+		collection: 'users',
+		data,
+	})
+
+	res.status(200).render('profile-page.ejs', {user})
+}
+
+module.exports = { getProfilePage, postProfilePage }
