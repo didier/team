4374d6501a1af6cab424e6d6a5d0f35250d054da
Sybren Molenaar <sybren.molenaar@hva.nl>
2020-06-19 15:21:48 +0200

    made changes in index.js

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const router = require('./src/router')
 
-const { PORT, NODE_ENV, SESSION_SECRET } = process.env
+const { PORT, NODE_ENV, SESSION_KEY } = process.env
 const port = PORT || 3000
 
 app
 	.use(express.static('public'))
 	.use(bodyParser.urlencoded({ extended: true }))
 	.use(cookieParser())
-	.use(session({ secret: SESSION_SECRET }))
+	.use(session({ secret: SESSION_KEY}))
 	.set('view engine', 'ejs')
 	.set('views', 'src/views')
 	.use(router)
--- src/routes/match.js
+++ src/routes/match.js
@@ -1,20 +1,20 @@
-const {	Read } = require('../db')
+// const {	Read } = require('../db')
 
-async function getMatch() {
+ async function getMatch() {
 
-	// krijg de sessie data van de ingelogde user
+// 	// krijg de sessie data van de ingelogde user
 
-	const user =  req.session.user;
+// 	const user =  req.session.user;
 
-	console.log(user);
+	console.log("Hallo, wij zijn team 2");
 
-	// maak de query
+// 	// maak de query
 
-	// haal de mogelijke matches uit de database
+// 	// haal de mogelijke matches uit de database
 
-	// maak je klaar om te laten zien om de match pagina
+// 	// maak je klaar om te laten zien om de match pagina
 
 
 }
 
-module.exports = { getMatch }
+ module.exports = { getMatch }
